745314e84105ce2865b7988bfe211cf5c99a1853
Ali Abbas <16102514+aarajput@users.noreply.github.com>
2021-06-04 18:42:03 +0500

    LAST_CONSUMPTION_TIMESTAMP added in member_update_reason.dart

--- lib/src/member_update_reason.dart
+++ lib/src/member_update_reason.dart
@@ -5,6 +5,9 @@ enum MemberUpdateReason {
   /// [Member] last consumed message index has changed.
   LAST_CONSUMED_MESSAGE_INDEX,
 
+  /// [Member] last consumption timestamp has changed.
+  LAST_CONSUMPTION_TIMESTAMP,
+
   /// This update will be documented in the next SDK version.
   ATTRIBUTES,
 }
